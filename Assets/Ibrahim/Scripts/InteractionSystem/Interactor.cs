4a51fc285a6353c96cf4e00962c3a295177632c3
iAltalhah <62838643+iAltalhah@users.noreply.github.com>
2026-05-05 22:10:40 +0300

    the interaction system is completed
    now I will try to add the rewind mechanic
    
    then ill start working on the items, doors, and the house interactions

--- Assets/Ibrahim/Scripts/InteractionSystem/Interactor.cs
+++ Assets/Ibrahim/Scripts/InteractionSystem/Interactor.cs
@@ -1,22 +1,48 @@
 using UnityEngine;
 
-interface IInteractable
-{
-    public void Interact();
-}
 public class Interactor : MonoBehaviour
 {
     [SerializeField] Transform interactionSource;
-    [SerializeField] float interactionRange;
-    [SerializeField] float interactionTime;
+    [SerializeField] float interactionRange = 2f;
+    [SerializeField] KeyCode interactionKey = KeyCode.E; // maybe we can change it
+    [SerializeField] InteractionPromptUI promptUI;
 
-    // Update is called once per frame
+    IInteractable currentInteractable; // to check which interactable is it
+
     void Update()
     {
-        Ray ray = new Ray(interactionSource.position, interactionSource.forward);
-        if(Physics.Raycast(ray,out RaycastHit hit, interactionRange))
+        CheckForInteractable();
+
+        if (currentInteractable != null && Input.GetKeyDown(interactionKey))
         {
-            Debug.Log(hit.collider.name);
+            currentInteractable.Interact();
+        }
+    }
+
+    void CheckForInteractable()
+    {
+        Ray ray = new Ray(interactionSource.position, interactionSource.forward); // create raycast and its range
+
+        if (Physics.Raycast(ray, out RaycastHit hit, interactionRange)) // hit is the object we are hitting with the ray
+        {
+            IInteractable interactable = hit.collider.GetComponentInParent<IInteractable>(); // we define a new var and assign the interactable conponent to it
+
+            if (interactable != null && interactable.CanInteract())
+            {
+                if (interactable != currentInteractable)
+                {
+                    currentInteractable = interactable;
+                    promptUI.ShowPrompt(interactable.InteractionPrompt);
+                }
+
+                return;
+            }
+        }
+
+        if (currentInteractable != null) // 
+        {
+            currentInteractable = null;
+            promptUI.HidePrompt();
         }
     }
 }
