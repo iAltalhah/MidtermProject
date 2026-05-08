44aad08bd8d892a6d85961f3af5dcc712547f0d3
iAltalhah <ialtalhah@gmail.com>
2026-05-08 09:29:18 +0300

    COLLIDER TO THE MAZE

--- Assets/Ibrahim/Scripts/GameManager.cs
+++ Assets/Ibrahim/Scripts/GameManager.cs
@@ -9,6 +9,8 @@ public class GameManager : MonoBehaviour
 
     [SerializeField] TextMeshProUGUI counterText;
 
+    [SerializeField] DayNightSkyboxBlender dayNightSkyboxBlender;
+
     float timer;
 
     private void Start()
@@ -19,6 +21,8 @@ public class GameManager : MonoBehaviour
     {
         timer -= Time.deltaTime;
         counterText.text = Mathf.CeilToInt(timer).ToString();
+
+        // ChangeDayCycle();
 
         // timer color changes when it decreases
         if (timer  < 30)
@@ -40,6 +44,35 @@ public class GameManager : MonoBehaviour
 
     public void ResetTheDay()
     {
+        dayNightSkyboxBlender.blendAmount = 0f;
         timer = dailyCountDown;
     }
+
+  /*  void ChangeDayCycle()
+    {
+        if (timer < 170 && timer > 161)
+        {
+            dayNightSkyboxBlender.blendAmount = 0.5f;
+        }
+        else if (timer < 160 && timer > 151)
+        {
+            dayNightSkyboxBlender.blendAmount = 0.4f;
+
+        }
+        else if (timer < 150 && timer > 141)
+        {
+            dayNightSkyboxBlender.blendAmount = 0.1f;
+
+        }
+        else if (timer < 140 && timer > 131)
+        {
+            dayNightSkyboxBlender.blendAmount = 1f;
+
+        }
+        else if (timer < 130 && timer > 121)
+        {
+            dayNightSkyboxBlender.blendAmount = 0.5f;
+
+        }
+    }*/
 }
